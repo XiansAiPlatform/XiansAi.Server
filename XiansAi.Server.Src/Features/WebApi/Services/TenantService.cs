4ec87634724db876246f398078e8d8557625b96d
Hasith Yaggahavita <hasith@gmail.com>
2025-08-31 14:50:47 +0530

    Update logging message in TenantService for clarity on access restrictions
    - Modified the warning log in TenantService to clarify that access is restricted to SysAdmins and the tenant ID, improving the readability and understanding of the log message.

--- XiansAi.Server.Src/Features/WebApi/Services/TenantService.cs
+++ XiansAi.Server.Src/Features/WebApi/Services/TenantService.cs
@@ -100,7 +100,7 @@ public class TenantService : ITenantService
         }
 
         // Otherwise, forbidden
-        _logger.LogWarning("Tenant admin attempted to access tenant {Id} but is restricted to their own tenant {TenantId}", tenantId, _tenantContext.TenantId);
+        _logger.LogWarning("Attempted to access tenant `{Id}` but is restricted to SysAdmins and of tenant `{TenantId}`", tenantId, _tenantContext.TenantId);
         throw new Exception(("Access denied: insufficient permissions"));
     }
 
